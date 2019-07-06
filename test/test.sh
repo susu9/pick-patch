d88a67cd7c003309dd79787498ae5fde23a6de16
Rick Chang <chchang915@gmail.com>
2019-07-05 19:09:06 -0700

    Fix auto test issue

--- test/test.sh
+++ test/test.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
+
+init_netrc() {
+    if [ -e ~/.netrc ]; then
+        return
+    fi
+    echo "Create ~/.netrc"
+    sudo echo "machine review.gerrithub.io login ${GERRITHUB_API_USER} password ${GERRITHUB_API_PASSWORD}" > ~/.netrc
+}
+
 testBasic() {
     python pick-patch --help
     assertEquals 0 $? 
 }
 
 testPick() {
-    if [ ! -e "~/.netrc" ]; then
-        python pick-patch -u susu9 -p ${GERRITHUB_API_PASSWORD} -g https://review.gerrithub.io -d -i . 460654
-    else
-        python pick-patch -g https://review.gerrithub.io -d -i . 460654
-    fi
+    init_netrc
+    python pick-patch -g https://review.gerrithub.io -d -i . 460654 -x 'git log --oneline -1'
     assertEquals 0 $? 
 }
+
