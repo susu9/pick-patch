38d78ac3923c0b41fbff07a7547cd2bcc55c45be
Rick Chang <chchang915@gmail.com>
2020-01-19 22:35:33 -0800

    test: add test case for config

--- test/test.sh
+++ test/test.sh
@@ -25,3 +25,18 @@ testQuery() {
     python pick-patch -g https://review.gerrithub.io -d -i . -q 'owner:"Rick chang" after:"2019-07-05 22:00:00 -0700"'
     assertEquals 0 $?
 }
+
+init_pickrc() {
+    if [ -e ~/.pickrc ]; then
+        return
+    fi
+    echo "Create ~/.pickrc"
+    sudo sed -e "s/$USER/${GERRITHUB_API_USER}/" -e "s/$PASSWORD/${GERRITHUB_API_PASSWORD}/" test/pickrc.test > ~/.pickrc
+}
+
+testConfig() {
+    init_netrc
+    init_pickrc
+    python pick-patch -d -i . 460654/2 482097
+    assertEquals 0 $?
+}
